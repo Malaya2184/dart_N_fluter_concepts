f6a86df2c8de2ce8b78c089eb56aeeff5d771dda
Malaya2184 <malayakumarswain8@gmail.com>
2021-09-11 03:02:04 +0530

    function in  dart

--- dart_basics/06_dartbasic.dart
+++ dart_basics/06_dartbasic.dart
@@ -1,3 +1,19 @@
+//functions in dart
+
+void helloworld(){
+  print('hello world');
+}
+//type of parameter passing
+int add(a,b){
+
+return a+b;
+}
+
+int add1(int a, int b){
+  return a+b;
+}
 main() {
-   
+   helloworld();
+   print(add(5,6));
+   print(add1(5,6));
 }
