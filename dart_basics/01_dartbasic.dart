4879d631427bde3d38aafe8e07679c134fc53c16
Malaya2184 <malayakumarswain8@gmail.com>
2021-09-10 21:43:48 +0530

    string pre defined

---
+++ dart_basics/01_dartbasic.dart
@@ -0,0 +1,20 @@
+// data types in dart
+main() {
+  print("hello world");
+  String firstName = " spider";
+  print("my firsrt name is ${firstName}");
+  // int age = 23;
+   double age = 23.5;
+  print("my age  is ${age}");
+
+  // need of ${} only inside the tring
+  bool istrue = false;
+  print(istrue);
+
+  String my_name = "My name is spider";
+  print(my_name[0]);
+  for (var i = 0; i < my_name.length; i++) {
+    print(my_name[i]);
+  }
+
+}
---
+++ dart_basics/02_dartbasic.dart
@@ -0,0 +1,28 @@
+// string predefined methods
+
+main(List<String> args) {
+  String my_name = "  My name is spider spider  ";
+  String your_name = "  My name is spider   ";
+  int my_num = 96589658;
+  print(my_name.length);
+  print(my_name.toLowerCase());
+  print(my_name.toUpperCase());
+  print(my_name.trim());
+  print(my_name.substring(2));
+  print(my_name.substring(3,5));
+  print(my_name.codeUnitAt(3));
+  print(my_name.isEmpty);
+
+  // 0 − when the strings are equal.
+
+  // 1 − when the first string is greater than the second
+
+  // -1 − when the first string is smaller than the second
+  print(my_name.compareTo(your_name));
+  // Replaces all substrings that match the specified pattern with a given value.
+  print(my_name.replaceAll("spider","spider-ms"));
+  print(my_num.toString());
+  String my_num_string = my_num.toString();
+  print(my_num.runtimeType);
+  print(my_num_string.runtimeType);
+}
