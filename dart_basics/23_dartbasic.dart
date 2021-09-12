8071f22d0ab268a58c2f3608a6ce47637468b141
Malaya2184 <malayakumarswain8@gmail.com>
2021-09-12 23:25:21 +0530

    generic map/ associative arry

---
+++ dart_basics/23_dartbasic.dart
@@ -0,0 +1,12 @@
+// generic map in dart
+
+void main() {
+  Map<String, String> m = {'name': 'Tom', 'Id': 'E1001'};
+  print('Map :${m}');
+
+  //map ==nassociative array below
+
+  var myassoc = {'name': 'maalaya kumar swain', 'age': 22};
+  print(myassoc['name']);
+  print(myassoc['age']);
+}
